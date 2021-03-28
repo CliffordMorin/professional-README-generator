87ace8741d457cbc18571e5d732a9236e81c828b
Clifford Morin <cemorin21@gmail.com>
2021-03-28 12:29:17 -0400

    added questions and validation function

---
+++ Develop/index.js
@@ -0,0 +1,104 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+// TODO: Create an array of questions for user input
+const questions = [
+      // GitHub Username
+      {
+        type: 'input',
+        name: 'username',
+        message: 'Enter your GitHub username.',
+        // We need to validate that user entered at least one word
+        // https://stackoverflow.com/questions/57321266/how-to-test-inquirer-validation
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("You must enter a GitHub username.");
+            }
+            return true;
+        }
+    },
+    // GitHub Repository
+    {
+        type: 'input',
+        name: 'repository',
+        message: 'Enter the name of your repository on GitHub.',
+        // We need to validate that user entered at least one word
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("You must enter the name of your GitHub repository.");
+            }
+            return true;
+        } 
+    },
+      // Title of Project
+      {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title of your project.',
+        // We need to validate that user entered at least one word
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("You must enter the title of your project.");
+            }
+            return true;
+        }
+    },
+       // Project Description
+       {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your project.',
+        // We need to validate that user entered at least one word
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("You must enter a description for your project.");
+            }
+            return true;
+        }
+    },
+    // Project Installation
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Explain how user would install (if necessary) for Installation Section.',
+        // Validation not required if question is optional
+    },
+    // Usage of project
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter your project instructions and examples of it in use for Usage Section.',
+        // Validation not required if question is optional
+    },
+    // Select license
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose your license for your project.',
+        // https://docs.github.com/en/free-pro-team@latest/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+        choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mit', 'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib']
+        
+    },
+    // Contributing to project
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Explain how users can contribute to your project (if necessary).',
+        // Validation not required if question is optional
+    },
+    // Test for project
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide tests for project, and explain how to test (if necessary).',
+        // Validation not required if question is optional
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
