bba62e7412f0fa878571bf89a53ec13e40208dd0
Clifford Morin <cemorin21@gmail.com>
2021-04-01 04:04:43 -0400

    added badge and link for linkedIn

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,16 +2,16 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === "apache-2.0") {
-    return `[![apache-2.0](https://img.shields.io/badge/license-apache--2.0-green)]`
+    return `[![apache-2.0](https://img.shields.io/badge/license-apache--2.0-green?style=plastic)]`
   }
   else if (license === 'BSD 3-Clause "New" or "Revised"'){
-    return `[![BSD 3-Clause "New" or "Revised"](https://img.shields.io/badge/license-BSD%203--Clause%20%22New%22%20or%20%22Revised%22-green)]`
+    return `[![BSD 3-Clause "New" or "Revised"](https://img.shields.io/badge/license-BSD%203--Clause%20%22New%22%20or%20%22Revised%22-green?style=plastic)]`
   }
   else if (license === 'GNU General Public License v3.0'){
-    return `[![GNU General Public License v3.0](https://img.shields.io/badge/license-GNU%20General%20Public%20License%20v3.0-green)]`
+    return `[![GNU General Public License v3.0](https://img.shields.io/badge/license-GNU%20General%20Public%20License%20v3.0-green?style=plastic)]`
   }
   else if (license === 'MIT'){
-    return `[![MIT](https://img.shields.io/badge/license-MIT-green)]`
+    return `[![MIT](https://img.shields.io/badge/license-MIT-green?style=plastic)]`
   }
   else {
     return ""
@@ -97,7 +97,8 @@ ${renderLicenseSection(data.license)}
 
 ## Questions
 
-  [${data.username} Github](https://github.com/${data.username})
+  [![GitHub](https://img.shields.io/badge/My%20GitHub-Click%20Me!-blueviolet?style=plastic&logo=GitHub)](https://github.com/${data.username}) 
+  [![LinkedIn](https://img.shields.io/badge/My%20LinkedIn-Click%20Me!-blue?style=plastic&logo=LinkedIn)](https://www.linkedin.com/in/morin-clifford-129888a9/)
 
   Feel free to reach me at ${data.email} with any question regarding this project!
   `;
